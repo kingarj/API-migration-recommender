59c92b01416e1b3a8d275fb3cb61d2228614e82d
kingarj <alicerjk@gmail.com>
2019-03-23 11:57:48 +0000

    write output to txt file

--- src/main/java/controllers/UserInterface.java
+++ src/main/java/controllers/UserInterface.java
@@ -1,16 +1,21 @@
 package controllers;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Scanner;
 
 import org.apache.http.client.ClientProtocolException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserInterface {
 
 	public static Controller controller;
+	private static Logger logger = LoggerFactory.getLogger(UserInterface.class);
 
 	public UserInterface() {
 	}
@@ -18,12 +23,13 @@ public class UserInterface {
 	public static void main(String[] args) throws ClientProtocolException, URISyntaxException, IOException {
 
 		Scanner scanner = new Scanner(System.in);
+		String root = "src/main/resources/";
 
 		System.out.println("Please enter your GitHub username");
 		String username = scanner.nextLine();
 		System.out.println("Please enter your GitHub password");
 		String password = scanner.nextLine();
-		controller = new Controller("src/main/resources/libraries/", username, password);
+		controller = new Controller(root + "libraries/", username, password);
 
 		while (true) {
 			System.out.println("Enter x to exit at any time");
@@ -41,8 +47,21 @@ public class UserInterface {
 
 			HashMap<String, String> recommendations = controller.generateRecommendations(source, target);
 
-			for (Entry<String, String> r : recommendations.entrySet()) {
-				System.out.println("We recommend that you map:\n " + r.getKey() + "\n" + r.getValue() + "\n");
+			try {
+
+				PrintWriter writer = new PrintWriter(root + "output/" + System.currentTimeMillis() + ".txt");
+
+				for (Entry<String, String> r : recommendations.entrySet()) {
+					writer.println("We recommend that you map: ");
+					writer.println(r.getKey());
+					writer.println("to");
+					writer.println("\n" + r.getValue());
+					writer.println();
+					;
+				}
+				writer.close();
+			} catch (FileNotFoundException e) {
+				logger.debug(e.getMessage());
 			}
 		}
 		scanner.close();
